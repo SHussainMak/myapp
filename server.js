1da9978f5a5f1450fd9e55c9bcfb77843325deec
I330413 <saddamhussain.makandar@sap.com>
2018-10-28 19:43:11 +0530

    Initial commit-3

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ if (!dev) {
 
     app.use(express.static(path.resolve(__dirname, 'build')));
 
-    app.get("/*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    app.get("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'src/index.js'));
     });
 }
 
