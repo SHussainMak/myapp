f31791ddc4dcd4e32606b0c4842b8276491f43dc
I330413 <saddamhussain.makandar@sap.com>
2018-11-14 13:47:48 +0530

    Added build

--- server.js
+++ server.js
@@ -6,20 +6,18 @@ const path = require('path');
 
 // process.env.NODE_ENV = 'production';
 
-// const normalizePort = port => parseInt(port , 10);
-// const PORT = normalizePort(process.env.PORT || 5000);
+const normalizePort = port => parseInt(port , 10);
+const PORT = normalizePort(process.env.PORT || 5000);
 
 const app = express();
 const dev = app.get('evn') !== 'production';
 
-app.set('port', process.env.PORT || 5000);
-
 if (!dev) {
-    app.disable('x-powered-by');
-    app.use(compression());
-    app.use(morgan('common'));
+    // app.disable('x-powered-by');
+    // app.use(compression());
+    // app.use(morgan('common'));
 
-    app.use(express.static(path.join(__dirname+'build')));
+    // app.use(express.static(path.join(__dirname+'build')));
 
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname + 'build/index.html'));
@@ -32,7 +30,7 @@ if(dev) {
 
 const server = createServer(app);
 
-server.listen(app.get('port'), err => {
+server.listen(PORT, err => {
     if (err) throw err;
 
     console.log("server started");
